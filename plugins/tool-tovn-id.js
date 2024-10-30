128b2ceb2b3634a19977c0973bd10bd09a6b0988
Rapikz-kwontwol <rafiqmarwan100@gmail.com>
2024-10-31 05:56:33 +0700

    Update tool-tovn-id.js

--- plugins/tool-tovn-id.js
+++ plugins/tool-tovn-id.js
@@ -18,5 +18,6 @@ let handler = async (m, { conn, usedPrefix, command, text }) => {
 handler.help = ['tovn-id'];
 handler.command = /^(tovn-id)$/i;
 handler.tags = ['tools'];
+handler.owner = true
 
 export default handler;
