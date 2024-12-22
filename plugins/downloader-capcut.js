e75437bff1871ad4c37ef046aed7c6875149dd7c
Rapikz-kwontwol <rafiqmarwan100@gmail.com>
2024-12-23 06:43:59 +0700

    Update downloader-capcut.js

--- plugins/downloader-capcut.js
+++ plugins/downloader-capcut.js
@@ -3,7 +3,7 @@ import fetch from 'node-fetch'
  let handler = async (m, { conn, text, usedPrefix, command }) => {
    if (!text) throw `*Example:* ${usedPrefix}${command} https://www.capcut.com/t/Zs8hVDbPf/`;
 conn.sendMessage(m.chat, { react: { text: '🕒', key: m.key }})
- let ouh = await fetch(`https://widipe.com/download/capcut?url=${text}`)
+ let ouh = await fetch(`https://btch.us.kg/download/capcut?url=${text}`)
   let gyh = await ouh.json() 
 	let url = `${gyh.result.video_ori}`
 	conn.sendFile(m.chat, url, null, `*D E S K R I P S I :*\n${gyh.result.description}\nDIGUNAKAN SEBANYAK : ${gyh.result.digunakan}`, m)
