d239cb3312bbe43b5972f7911108e46c50c2a603
Rapikz-kwontwol <rafiqmarwan100@gmail.com>
2024-12-23 06:46:03 +0700

    Update downloader-facebook.js

--- plugins/downloader-facebook.js
+++ plugins/downloader-facebook.js
@@ -3,7 +3,7 @@ import fetch from 'node-fetch'
  let handler = async (m, { conn, text, usedPrefix, command }) => {
    if (!text) throw `*Example:* ${usedPrefix}${command} https://www.facebook.com/100077779900033/videos/1526198498276164/?mibextid=rS40aB7S9Ucbxw6v`;
 conn.sendMessage(m.chat, { react: { text: '🕒', key: m.key }})
- let ouh = await fetch(`https://widipe.com/download/fbdl?url=${text}`)
+ let ouh = await fetch(`https://btch.us.kg/download/fbdl?url=${text}`)
   let gyh = await ouh.json() 
 	let url = `${gyh.result.HD}`
 	conn.sendFile(m.chat, url, null, `nih videonya kak`, m)
