4d0bdade685377e30cdbea3346ea32fbd642e736
Rapikz-kwontwol <rafiqmarwan100@gmail.com>
2024-12-23 17:25:56 +0700

    Create info-script.js

---
+++ plugins/info-script.js
@@ -0,0 +1,10 @@
+let handler = async (m) => {
+
+let anu = `https://github.com/Rapikz-kwontwol/SC-Bot-WA-Ajah`
+await m.reply(anu)
+}
+handler.help = ['sc','script']
+handler.tags = ['info']
+handler.command = /^(sc|script)$/i
+
+export default handler
