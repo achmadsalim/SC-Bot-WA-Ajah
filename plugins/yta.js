38b298d55159bc9647f421e5bda47c9dc2c97d15
Rapikz-kwontwol <rafiqmarwan100@gmail.com>
2024-12-24 23:32:52 +0700

    Update yta.js

--- plugins/yta.js
+++ plugins/yta.js
@@ -1,3 +1,4 @@
+/*
 import fetch from 'node-fetch'
 import axios from 'axios'
 async function yta(url) {
@@ -33,3 +34,33 @@ handler.limit = true
 handler.register = true
 
 export default handler
+*/
+
+import fetch from 'node-fetch'
+import axios from 'axios'
+
+let handler = async (m, { conn, usedPrefix, args, command, text }) => {
+if (!text) throw `Linknya Mana?`
+m.reply("*YTMP3 DOWNLOAD IN PROGRESS*\n\n\nTunggu yak")
+try {
+let anu = await fetch(`https://api.betabotz.eu.org/api/download/yt?url=${text}&apikey=btzziaulhaq`)
+let gatau = await anu.json()   
+let linknyah = gatau.result.mp3
+await conn.sendMessage(m.chat, {
+      audio: { url: linknyah },
+      mimetype: 'audio/mpeg'
+    }, { quoted: m });
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
+};
+
+handler.help = ['yta']
+handler.tags = ['downloader']
+handler.command = /^(yta|ytmp3|ytaudio)$/i
+handler.limit = true
+handler.register = true
+
+export default handler
+
