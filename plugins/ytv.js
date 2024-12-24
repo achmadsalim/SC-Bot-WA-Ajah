8fa913b65b0b3f98c5264d8fe908c58fc880cdcc
Rapikz-kwontwol <rafiqmarwan100@gmail.com>
2024-12-24 23:40:34 +0700

    Update ytv.js

--- plugins/ytv.js
+++ plugins/ytv.js
@@ -1,20 +1,10 @@
 import fetch from 'node-fetch'
-import axios from 'axios'
-
-async function ytv(url) {
-    const response = await axios.post('https://dl.excdn.us.kg/video', { url }, {
-        headers: {
-            'Accept': 'application/json',
-            'Content-Type': 'application/json',
-        },
-    });
-    return response.data
-}
 let handler = async (m, { conn, usedPrefix, args, command, text }) => {
 if (!text) throw `Linknya Mana?`
 m.reply(wait)
-let anu = await ytv(text)
-let linknyah = anu.fileUrl
+let anu = await fetch(`https://api.betabotz.eu.org/api/download/yt?url=${text}&apikey=btzziaulhaq`)
+let apayah = await anu.json()    
+let linknyah = apayah.result.mp4
 conn.sendFile(m.chat, linknyah, null, 'Nih vidnya kak', m)
 }
 handler.help = ['ytv']
@@ -22,5 +12,4 @@ handler.tags = ['downloader']
 handler.command = /^(ytv|ytvideo|ytmp4)$/i
 handler.limit = true
 handler.register = true
-
 export default handler
