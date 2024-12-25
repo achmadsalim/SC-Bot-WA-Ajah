37ddf1e5362d50d75cbd78e6ac44df1c09a292ba
Rapikz-kwontwol <rafiqmarwan100@gmail.com>
2024-12-25 10:42:30 +0700

    Create ttp.js

---
+++ plugins/ttp.js
@@ -0,0 +1,14 @@
+import { sticker } from '../lib/sticker.js'
+
+let handler = async (m, { conn, text, usedPrefix, command }) => {
+if (!text) throw `Contoh penggunaan: ${usedPrefix + command} Crot Crot`;
+let stiker = await sticker(null, `https://btch.us.kg/ttp?text=${text}`, '', '')
+conn.sendFile(m.chat, stiker, 'sticker.webp', '', m)  
+} 
+
+handler.help = ['ttp']
+handler.tags = ['sticker']
+handler.command = /^(ttp)$/i
+handler.limit = 10 //biar gak spam
+handler.register = true // biar daftar dlu
+export default handler
