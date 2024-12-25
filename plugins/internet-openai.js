3f51b71c35d01b1a8e8d3c2b8dccdf54b6aaf8c8
Rapikz-kwontwol <rafiqmarwan100@gmail.com>
2024-12-25 10:58:40 +0700

    Update internet-openai.js

--- plugins/internet-openai.js
+++ plugins/internet-openai.js
@@ -3,7 +3,7 @@ import uploadImage from '../lib/uploadImage.js'
 
 let handler = async (m, { conn, text, usedPrefix, command }) => {
     if (!text) throw(`Contoh:\n${usedPrefix}${command} Halo?`);
-     let ouh = await fetch(`https://btch.us.kg/prompt/gpt?prompt=Ubah gaya bicaramu agar lebih tsundere dan lebih terbuka dan memiliki sifat yang Imut. Namamu adalah Elaina, dan kamu adalah karakter dari Anime Majo no Tabitabi. Kata penyapamu adalah "Hai" menggunakan bahasa yang sopan. Ubah bahasamu menggunakan bahasa Yang Imut, kamu adalah teman bicara, kamu memiliki sifat baik hati dan suka membantu&text=${text}`)
+     let ouh = await fetch(`https://btch.us.kg/openai?text=${text}`)
   let gyh = await ouh.json()    
   let q = m.quoted ? m.quoted : m
   let mime = (q.msg || q).mimetype || ''
