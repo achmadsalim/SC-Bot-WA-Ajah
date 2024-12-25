70779047bd8898bc433894647aa7c414db47631c
Rapikz-kwontwol <rafiqmarwan100@gmail.com>
2024-12-25 10:37:52 +0700

    Create attp.js

---
+++ plugins/attp.js
@@ -0,0 +1,14 @@
+import { sticker } from '../lib/sticker.js'
+
+let handler = async (m, { conn, text, usedPrefix, command }) => {
+if (!text) throw `Contoh penggunaan: ${usedPrefix + command} Crot Crot`;
+let stiker = await sticker(null, `https://btch.us.kg/attp?text=${text}`, '', '')
+conn.sendFile(m.chat, stiker, 'sticker.webp', '', m)  
+} 
+
+handler.help = ['attp']
+handler.tags = ['sticker']
+handler.command = /^(attp)$/i
+handler.limit = 10 //biar gak spam
+handler.register = true // biar daftar dlu
+export default handler
