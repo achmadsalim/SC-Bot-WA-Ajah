6c04e33c2b84fbbfa3a96d985c7e7bd084125ca7
Rapikz-kwontwol <rafiqmarwan100@gmail.com>
2024-12-25 18:54:47 +0700

    HaSiL COpAs WaK, tAPi yAnG peNTing WoRK

--- plugins/downloader-play2.js
+++ plugins/downloader-play2.js
@@ -1,72 +1,8 @@
-// Play YouTube By Mana Ku tw
-// screp : https://whatsapp.com/channel/0029VavOkL00lwgmRLmffH1i/159
+// YAYAYA SERAH LU ASAL JAN TEMPEL2 WM KAU AJA
+// SCRAPER DARI : https://whatsapp.com/channel/0029VavOkL00lwgmRLmffH1i/159
 import axios from "axios";
 import yts from "yt-search";
 import fetch from 'node-fetch';
-
-const formatAudio = ['mp3', 'm4a', 'webm', 'acc', 'flac', 'opus', 'ogg', 'wav'];
-const formatVideo = ['360', '480', '720', '1080', '1440', '4k'];
-
-const ddownr = {
-  download: async (url, format) => {
-    if (!formatAudio.includes(format) && !formatVideo.includes(format)) {
-      throw new Error('Format tidak didukung, cek daftar format yang tersedia.');
-    }
-
-    const config = {
-      method: 'GET',
-      url: `https://p.oceansaver.in/ajax/download.php?format=${format}&url=${encodeURIComponent(url)}&api=dfcb6d76f2f6a9894gjkege8a4ab232222`,
-      headers: {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
-      }
-    };
-
-    try {
-      const response = await axios.request(config);
-
-      if (response.data && response.data.success) {
-        const { id, title, info } = response.data;
-        const { image } = info;
-        const downloadUrl = await ddownr.cekProgress(id);
-
-        return {
-          id: id,
-          image: image,
-          title: title,
-          downloadUrl: downloadUrl
-        };
-      } else {
-        throw new Error('Gagal mengambil detail video.');
-      }
-    } catch (error) {
-      console.error('Error:', error);
-      throw error;
-    }
-  },
-  cekProgress: async (id) => {
-    const config = {
-      method: 'GET',
-      url: `https://p.oceansaver.in/ajax/progress.php?id=${id}`,
-      headers: {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
-      }
-    };
-
-    try {
-      while (true) {
-        const response = await axios.request(config);
-
-        if (response.data && response.data.success && response.data.progress === 1000) {
-          return response.data.download_url;
-        }
-        await new Promise(resolve => setTimeout(resolve, 5000));
-      }
-    } catch (error) {
-      console.error('Error:', error);
-      throw error;
-    }
-  }
-};
 function formats(views) {
     if (views >= 1000000) {
         return (views / 1000000).toFixed(1).replace(/\.0$/, '') + 'M';
@@ -76,7 +12,6 @@ function formats(views) {
     }
     return views.toString();
 }
-
 const handler = async (m, { conn, text, usedPrefix }) => {
   if (!text) throw 'Masukkan Judul / Link Dari YouTube!';
   try {
@@ -93,7 +28,6 @@ const handler = async (m, { conn, text, usedPrefix }) => {
         conn.reply(m.chat, 'Tunggu sebentar...', m);
         audioUrl = await ddownr.download(video.url, "mp3");
       }
-
       let caption = '';
       caption += `∘ Judul : ${hasil.title}\n`;
       caption += `∘ Ext : Search\n`;
@@ -106,7 +40,6 @@ const handler = async (m, { conn, text, usedPrefix }) => {
       caption += `∘ Url : ${hasil.url}\n`;
       caption += `∘ Deskripsi : ${hasil.description}\n`;
       caption += `∘ Thumbnail : ${hasil.image}`;
-
       await conn.relayMessage(m.chat, {
         extendedTextMessage: {
           text: caption,
@@ -123,7 +56,6 @@ const handler = async (m, { conn, text, usedPrefix }) => {
           mentions: [m.sender]
         }
       }, {});
-
       await conn.sendMessage(m.chat, {
         audio: {
           url: audioUrl.downloadUrl
@@ -148,11 +80,66 @@ const handler = async (m, { conn, text, usedPrefix }) => {
     conn.reply(m.chat, `*Error:* ` + e.message, m);
   }
 };
-
 handler.command = handler.help = ['play2', 'song2'];
 handler.tags = ['downloader'];
 handler.exp = 0;
 handler.limit = true;
 handler.premium = false;
-
 export default handler;
+const formatAudio = ['mp3', 'm4a', 'webm', 'acc', 'flac', 'opus', 'ogg', 'wav'];
+const formatVideo = ['360', '480', '720', '1080', '1440', '4k'];
+const ddownr = {
+  download: async (url, format) => {
+    if (!formatAudio.includes(format) && !formatVideo.includes(format)) {
+      throw new Error('Format tidak didukung, cek daftar format yang tersedia.');
+    }
+    const config = {
+      method: 'GET',
+      url: `https://p.oceansaver.in/ajax/download.php?format=${format}&url=${encodeURIComponent(url)}&api=dfcb6d76f2f6a9894gjkege8a4ab232222`,
+      headers: {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
+      }
+    };
+    try {
+      const response = await axios.request(config);
+      if (response.data && response.data.success) {
+        const { id, title, info } = response.data;
+        const { image } = info;
+        const downloadUrl = await ddownr.cekProgress(id);
+        return {
+          id: id,
+          image: image,
+          title: title,
+          downloadUrl: downloadUrl
+        };
+      } else {
+        throw new Error('Gagal mengambil detail video.');
+      }
+    } catch (error) {
+      console.error('Error:', error);
+      throw error;
+    }
+  },
+  cekProgress: async (id) => {
+    const config = {
+      method: 'GET',
+      url: `https://p.oceansaver.in/ajax/progress.php?id=${id}`,
+      headers: {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
+      }
+    };
+    try {
+      while (true) {
+        const response = await axios.request(config);
+
+        if (response.data && response.data.success && response.data.progress === 1000) {
+          return response.data.download_url;
+        }
+        await new Promise(resolve => setTimeout(resolve, 5000));
+      }
+    } catch (error) {
+      console.error('Error:', error);
+      throw error;
+    }
+  }
+};
