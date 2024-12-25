3fecd4cfd7f0dde3246a9393962cd2b5197ef17e
Rapikz-kwontwol <rafiqmarwan100@gmail.com>
2024-12-25 18:44:55 +0700

    Create yta2.js

---
+++ plugins/yta2.js
@@ -0,0 +1,87 @@
+import axios from "axios";
+import yts from "yt-search";
+import fetch from 'node-fetch';
+let handler = async (m, { conn, usedPrefix, args, command, text }) => {
+if (!text) throw `Linknya Mana?`
+m.reply("*YTMP3 DOWNLOAD IN PROGRESS*\n\n\nTunggu yak")
+try {
+let anu = await ddownr.download(text, "mp3")
+let linknyah = anu.downloadUrl
+await conn.sendMessage(m.chat, {
+      audio: { url: linknyah },
+      mimetype: 'audio/mpeg'
+    }, { quoted: m });
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
+};
+handler.help = ['yta2']
+handler.tags = ['downloader']
+handler.command = /^(yta2|ytmp32|ytaudio2)$/i
+handler.limit = true
+handler.register = true
+export default handler
+// scrapernya dari : https://whatsapp.com/channel/0029VavOkL00lwgmRLmffH1i/159
+const formatAudio = ['mp3', 'm4a', 'webm', 'acc', 'flac', 'opus', 'ogg', 'wav'];
+const formatVideo = ['360', '480', '720', '1080', '1440', '4k'];
+const ddownr = {
+  download: async (url, format) => {
+    if (!formatAudio.includes(format) && !formatVideo.includes(format)) {
+      throw new Error('Format tidak didukung, cek daftar format yang tersedia.');
+    }
+
+    const config = {
+      method: 'GET',
+      url: `https://p.oceansaver.in/ajax/download.php?format=${format}&url=${encodeURIComponent(url)}&api=dfcb6d76f2f6a9894gjkege8a4ab232222`,
+      headers: {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
+      }
+    };
+
+    try {
+      const response = await axios.request(config);
+
+      if (response.data && response.data.success) {
+        const { id, title, info } = response.data;
+        const { image } = info;
+        const downloadUrl = await ddownr.cekProgress(id);
+
+        return {
+          id: id,
+          image: image,
+          title: title,
+          downloadUrl: downloadUrl
+        };
+      } else {
+        throw new Error('Gagal mengambil detail video.');
+      }
+    } catch (error) {
+      console.error('Error:', error);
+      throw error;
+    }
+  },
+  cekProgress: async (id) => {
+    const config = {
+      method: 'GET',
+      url: `https://p.oceansaver.in/ajax/progress.php?id=${id}`,
+      headers: {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
+      }
+    };
+
+    try {
+      while (true) {
+        const response = await axios.request(config);
+
+        if (response.data && response.data.success && response.data.progress === 1000) {
+          return response.data.download_url;
+        }
+        await new Promise(resolve => setTimeout(resolve, 5000));
+      }
+    } catch (error) {
+      console.error('Error:', error);
+      throw error;
+    }
+  }
+};
