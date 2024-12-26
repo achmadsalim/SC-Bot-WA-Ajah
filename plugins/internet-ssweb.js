52ac1ea4276c1f001eb911ad1e8a003fef9bbcdc
Rapikz-kwontwol <rafiqmarwan100@gmail.com>
2024-12-26 07:09:21 +0700

    Ganti domain rest api

--- plugins/internet-ssweb.js
+++ plugins/internet-ssweb.js
@@ -3,7 +3,7 @@ import fetch from 'node-fetch'
 let handler = async (m, { conn, text, usedPrefix }) => {
     if (!text) return m.reply("Linknya?");
     conn.sendMessage(m.chat, { react: { text: "⏳", key: m.key } });
-	let urls = `https://btch.us.kg/sspc?url=${text}`
+	let urls = `https://api.tioo.eu.org/sspc?url=${text}`
 	conn.sendFile(m.chat, urls, null, `hasil SS web dari web ${text}`, m)
 	await conn.sendMessage(m.chat, { react: { text: '✅', key: m.key }})
 }
